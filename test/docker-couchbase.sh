25244750269b0c39940e4e2660f2714d676821f2
chopperlee <ChopperLee2011@users.noreply.github.com>
2015-11-05 16:48:45 +0800

    setup circleCI

--- test/docker-couchbase.sh
+++ test/docker-couchbase.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
 
-docker pull $COUCHBASE
+docker pull couchbase/server
 
-docker run -d -p 8091:8091 $COUCHBASE
+docker run -d -p 8091:8091 couchbase/server
 
 CONTAINER_IP=`docker ps | awk 'NR==2{print $1}'`
 
@@ -11,7 +11,7 @@ docker ps
 
 echo $CONTAINER_IP
 
-docker run --rm --entrypoint=/opt/couchbase/bin/couchbase-cli $COUCHBASE \
+docker run --rm --entrypoint=/opt/couchbase/bin/couchbase-cli couchbase/server \
 bucket-create -c $CONTAINER_IP:8091 \
        --bucket=default \
        --bucket-type=couchbase \
